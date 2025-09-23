1b1d5348dd5d8641419040e69bad7ebd11f968ff
pksingh <pksingh69313@gmail.com>
2025-09-23 16:22:45 +0530

    update: updated linear search

--- Search_Algo/Linearsearch.cpp
+++ Search_Algo/Linearsearch.cpp
@@ -4,18 +4,26 @@ using namespace std;
 int LinearSearch(int arr[], int size, int target) {
     for (int x = 0; x < size; x++) {
         if (arr[x] == target) {
-            cout << "Found element at index: " << x << endl;
             return x;
         }
     }
-    cout << "Not Found!!" << endl;
     return -1;
 }
 
-int main () {
+int main() {
     int marks[5] = {9, 4, 2, 6, 10};
     int size = 5;
     int target = 12;
-    LinearSearch(marks, size, target);
+
+    int index = LinearSearch(marks, size, target);
+    if(index != -1)
+        cout << "Found element at index: " << index << endl;
+    else
+        cout << "Not Found!!" << endl;
+
     return 0;
 }
+
+// Minor CP-style improvement
+// Usually CP me cout function ke andar nahi rakhte; function sirf index return kare aur printing main me kare.
+// Reason: function ka kaam single responsibility ho → debugging & reuse easy ho jaaye.
