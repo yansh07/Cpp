ff768ec84bafc96cdf6f2b1376cd367cbf49b757
pksingh <pksingh69313@gmail.com>
2025-09-23 16:18:29 +0530

    update: updated linear search

--- Search_Algo/Linearsearch.cpp
+++ Search_Algo/Linearsearch.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    int marks[5] = {5, 8, 9, 2, 1};
-    int size = 5;
-    int target = 9;
-
+int LinearSearch(int arr[], int size, int target) {
     for (int x = 0; x < size; x++) {
-        if (marks[x] == target) {
-            cout << x << endl;
+        if (arr[x] == target) {
+            cout << "Found element at index: " << x << endl;
+            return x;
         }
     }
+    cout << "Not Found!!" << endl;
+    return -1;
+}
+
+int main () {
+    int marks[5] = {9, 4, 2, 6, 10};
+    int size = 5;
+    int target = 12;
+    LinearSearch(marks, size, target);
     return 0;
 }
